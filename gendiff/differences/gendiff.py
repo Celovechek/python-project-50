3a8570f138f405a160f3d99d9ccd0e7ded105e90
Celovechek <celovekvodichka@gmail.com>
2024-08-27 15:54:42 +0300

    add formatter

--- gendiff/differences/gendiff.py
+++ gendiff/differences/gendiff.py
@@ -1,9 +1,9 @@
 from .tools import to_dict, build_diff, stylish
 
 
-def generate_diff(f1: str, f2: str) -> str:
+def generate_diff(f1: str, f2: str, formatter=stylish) -> str:
     '''Generating of difference for 2 files'''
     file1 = to_dict(f1)
     file2 = to_dict(f2)
     diff = build_diff(file1, file2)
-    return f"{{\n{stylish(diff)}\n}}"
+    return f"{{\n{formatter(diff)}\n}}"
