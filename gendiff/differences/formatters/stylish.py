54357b7396be551efada30698984884e4ef7f008
Celovechek <celovekvodichka@gmail.com>
2024-09-29 14:37:11 +0300

    take stylish and wrapper to 1 level

--- gendiff/differences/formatters/stylish.py
+++ gendiff/differences/formatters/stylish.py
@@ -17,48 +17,50 @@ def format_value(value, depth: int) -> str:
         return str(value)
 
 
+def format_diff(diff: dict, depth=0) -> str:
+    '''Helper function to format diff recursively'''
+    lines = []
+    space = '    ' * depth
+    for key, value in sorted(diff.items()):
+        diff_type = value.get('type')
+
+        if diff_type == 'include':
+            children = format_diff(value['children'], depth + 1)
+            lines.append(f'{space}    {key}: {{\n{children}\n{space}    }}')
+        elif diff_type == 'added':
+            new_value = format_value(value['value'], depth + 1)
+            lines.append(f'{space}  + {key}: {new_value}')
+        elif diff_type == 'removed':
+            old_value = format_value(value['value'], depth + 1)
+            lines.append(f'{space}  - {key}: {old_value}')
+        elif diff_type == 'changed dict to not dict':
+            children = format_diff(value['old_value'], depth + 1)
+            lines.append(f'{space}  - {key}: {{\n{children}\n{space}    }}')
+            new_value = format_value(value['new_value'], depth + 1)
+            lines.append(f'{space}  + {key}: {new_value}')
+        elif diff_type == 'changed not dict to dict':
+            old_value = format_value(value['old_value'], depth + 1)
+            lines.append(f'{space}  - {key}: {old_value}')
+            children = format_diff(value['new_value'], depth + 1)
+            lines.append(f'{space}  + {key}: {{\n{children}\n{space}    }}')
+        elif diff_type == 'changed not dict to not dict':
+            old_value = format_value(value['old_value'], depth + 1)
+            new_value = format_value(value['new_value'], depth + 1)
+            lines.append(f'{space}  - {key}: {old_value}')
+            lines.append(f'{space}  + {key}: {new_value}')
+        elif diff_type == 'unchanged':
+            unchanged_value = format_value(value['value'], depth + 1)
+            lines.append(f'{space}    {key}: {unchanged_value}')
+        elif diff_type == 'added dict':
+            children = format_diff(value['children'], depth + 1)
+            lines.append(f'{space}  + {key}: {{\n{children}\n{space}    }}')
+        elif diff_type == 'removed dict':
+            children = format_diff(value['children'], depth + 1)
+            lines.append(f'{space}  - {key}: {{\n{children}\n{space}    }}')
+
+    return '\n'.join(lines)
+
+
 def stylish(diff: dict, depth=0) -> str:
     '''Styling of diff'''
-
-    def wrapper(diff: dict, depth=0) -> str:
-        lines = []
-        space = '    ' * depth
-        for key, value in sorted(diff.items()):
-
-            diff_type = value.get('type')
-
-            if diff_type == 'include':
-                children = wrapper(value['children'], depth + 1)
-                lines.append(f'{space}    {key}: {{\n{children}\n{space}    }}')
-            elif diff_type == 'added':
-                new_value = format_value(value['value'], depth + 1)
-                lines.append(f'{space}  + {key}: {new_value}')
-            elif diff_type == 'removed':
-                old_value = format_value(value['value'], depth + 1)
-                lines.append(f'{space}  - {key}: {old_value}')
-            elif diff_type == 'changed dict to not dict':
-                children = wrapper(value['old_value'], depth + 1)
-                lines.append(f'{space}  - {key}: {{\n{children}\n{space}    }}')
-                new_value = format_value(value['new_value'], depth + 1)
-                lines.append(f'{space}  + {key}: {new_value}')
-            elif diff_type == 'changed not dict to dict':
-                old_value = format_value(value['old_value'], depth + 1)
-                lines.append(f'{space}  - {key}: {old_value}')
-                children = wrapper(value['new_value'], depth + 1)
-                lines.append(f'{space}  + {key}: {{\n{children}\n{space}    }}')
-            elif diff_type == 'changed not dict to not dict':
-                old_value = format_value(value['old_value'], depth + 1)
-                new_value = format_value(value['new_value'], depth + 1)
-                lines.append(f'{space}  - {key}: {old_value}')
-                lines.append(f'{space}  + {key}: {new_value}')
-            elif diff_type == 'unchanged':
-                unchanged_value = format_value(value['value'], depth + 1)
-                lines.append(f'{space}    {key}: {unchanged_value}')
-            elif diff_type == 'added dict':
-                children = wrapper(value['children'], depth + 1)
-                lines.append(f'{space}  + {key}: {{\n{children}\n{space}    }}')
-            elif diff_type == 'removed dict':
-                children = wrapper(value['children'], depth + 1)
-                lines.append(f'{space}  - {key}: {{\n{children}\n{space}    }}')
-        return '\n'.join(lines)
-    return f"{{\n{wrapper(diff, depth)}\n}}"
+    return f"{{\n{format_diff(diff, depth)}\n}}"
